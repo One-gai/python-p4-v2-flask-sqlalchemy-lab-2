9dac8de6ad89f6b607fbf602a345e6f8232a44b5
Luke <luke.wangai@student.moringaschool.com>
2025-06-18 15:19:44 +0300

    sqlalchemy lab 2 complete

--- server/models.py
+++ server/models.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy.ext.associationproxy import association_proxy
-from sqlalchemy_serializer import SerializerMixin
+
 
 
 metadata = MetaData(naming_convention={
@@ -16,9 +16,18 @@ class Customer(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    
+    reviews = db.relationship('Review', back_populates='customer')
 
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
+    
+    def to_dict(self):
+            return {
+                "id": self.id,
+                "name": self.name,
+                # Add "items": [i.to_dict() for i in self.items] if using relationships
+            }    
 
 
 class Item(db.Model):
@@ -28,5 +37,36 @@ class Item(db.Model):
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    reviews = db.relationship('Review', back_populates='item')
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "price": self.price,
+        }
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    customer = db.relationship('Customer', back_populates='reviews')
+    item = db.relationship('Item', back_populates='reviews')
+
+    def __repr__(self):
+        return f"<Review {self.id}: {self.comment}>"
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "comment": self.comment,
+            "customer_id": self.customer_id,
+            "item_id": self.item_id
+        }
